7f22c008efaa19aacc979e3af0d6d756c5a646b1
Basileusesport <basileusesport@gmail.com>
2023-10-30 21:37:32 +0100

    New form recrutement lié a l'adresse mail

--- site/ressources/script.js
+++ site/ressources/script.js
@@ -1,5 +1,35 @@
-let button = document.querySelector(".buttonhistoire");
+let buttonHistoire = document.querySelector(".buttonhistoire");
 
-button.addEventListener("click", () => {
+buttonHistoire.addEventListener("click", () => {
     location.replace("#");
+});
+
+let buttonRecrutement = document.querySelector(".envoyer-accueil-form");
+
+class InfoRecrutement {
+    constructor(username, discord, rank) {
+        this.username = username;
+        this.discord = discord;
+        this.rank = rank;
+    };
+
+    displayInfos() {
+        return `Son nom d'utilisateur : ${infoRecrutement.username} <br> Son Discord : ${infoRecrutement.discord} <br> Son rang : ${infoRecrutement.rank}`
+    };
+};
+
+const infoRecruté = new InfoRecrutement(document.querySelector("#username-accueil-form").value, document.querySelector("#discord-accueil-form").value, document.querySelector("#rank-accueil-form").value);
+
+buttonRecrutement.addEventListener("click", () => {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "kill.maldeme@gmail.com",
+        Password : "445BBCBD956CB9889327222ACE98F67C3835",
+        To : 'basileusesport@gmail.com',
+        From : "kill.maldeme@gmail.com",
+        Subject : `Nouveau recrutement de ${infoRecruté.discord} !`,
+        Body : `Il semblerait qu'une personne s'est inscrite sur notre site ! <br> ${infoRecruté.displayInfos()}`
+    }).then(
+      message => alert(message)
+    );
 });
